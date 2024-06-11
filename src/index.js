2b915ff7b5b4294107f2a272c6de821805fe81b8
JX Wang <wjianxu@gmail.com>
2024-06-11 19:18:25 +0800

    修改域名映射

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.someok.com": "https://registry-1.docker.io",
+  "quay.someok.com": "https://quay.io",
+  "gcr.someok.com": "https://gcr.io",
+  "k8s-gcr.someok.com": "https://k8s.gcr.io",
+  "k8s.someok.com": "https://registry.k8s.io",
+  "ghcr.someok.com": "https://ghcr.io",
+  "cloudsmith.someok.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
